1d3755270fb534a02b9bea0b58f185eb540bba8f
Thang Le Quang <lethang7794@gmail.com>
2021-01-21 18:12:14 +0700

    Use useEffects to fetchJobs and dispatch JOBS_FETCH_SUCCESS action

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import fetchJobs from './redux/actions/job.actions';
 import './App.css';
 import MainNavBar from './components/MainNavBar';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchJobs());
+  }, [dispatch]);
   return (
     <div className='App'>
       <MainNavBar />
