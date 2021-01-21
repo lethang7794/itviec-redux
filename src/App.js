337f9b7d326c1c39cce002bb64ac0a7959d76697
Thang Le Quang <lethang7794@gmail.com>
2021-01-21 18:13:28 +0700

    Use useSelector to get the jobs from Redux's state and render the jobs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import fetchJobs from './redux/actions/job.actions';
 import './App.css';
 import MainNavBar from './components/MainNavBar';
 
 function App() {
   const dispatch = useDispatch();
+  const jobs = useSelector((state) => state.jobs);
+
   useEffect(() => {
     dispatch(fetchJobs());
   }, [dispatch]);
@@ -16,6 +18,11 @@ function App() {
       <MainNavBar />
       <Container>
         <h1 className='text-center'>Let's go</h1>
+        <ul>
+          {jobs.map((job) => (
+            <li key={job.id}>{job.title}</li>
+          ))}
+        </ul>
       </Container>
     </div>
   );
