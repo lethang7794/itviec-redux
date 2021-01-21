d4afb7c09bf691666bb880f1d56b6e7f097191a8
Thang Le Quang <lethang7794@gmail.com>
2021-01-21 21:01:45 +0700

    Add routes for JobDetailPage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MainNavBar from './components/MainNavBar';
 import JobListPage from './pages/JobListPage';
+import JobDetailPage from './pages/JobDetailPage';
 
 function App() {
   return (
     <div className='App'>
       <MainNavBar />
-      <JobListPage />
+      <Switch>
+        <Route exact path='/' component={JobListPage} />
+        <Route path='/jobs/:id' component={JobDetailPage} />
+      </Switch>
     </div>
   );
 }
