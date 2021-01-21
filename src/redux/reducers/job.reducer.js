cc2d412fe0641f290bd5707ff9bfb40fee498e10
Thang Le Quang <lethang7794@gmail.com>
2021-01-21 18:11:05 +0700

    Make jobReducer handle JOBS_FETCH_SUCCESS action

--- src/redux/reducers/job.reducer.js
+++ src/redux/reducers/job.reducer.js
@@ -1,4 +1,15 @@
-const jobReducer = (state, action) => {
+const initialState = {
+  jobs: [],
+};
+
+const jobReducer = (state = initialState, action) => {
+  if (action.type === 'JOBS_FETCH_SUCCESS') {
+    return {
+      ...state,
+      jobs: action.payload,
+    };
+  }
+
   return state;
 };
 
