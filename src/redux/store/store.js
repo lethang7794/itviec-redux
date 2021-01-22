de06dc2da440a4cc312f771402a268fdcd12c612
Thang Le Quang <lethang7794@gmail.com>
2021-01-22 13:21:20 +0700

    Install and set up redux-devtools-extension
    https://github.com/zalmoxisus/redux-devtools-extension#13-use-redux-devtools-extension-package-from-npm

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,7 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import jobReducer from '../reducers/job.reducer';
 
-const store = createStore(jobReducer, applyMiddleware(thunk));
+const store = createStore(
+  jobReducer,
+  /* preloadedState, */
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
