40db103de857f55efe41f6caa1eb3575b2687dab
Thang Le Quang <lethang7794@gmail.com>
2021-01-22 12:03:08 +0700

    Use useDispatch to dispatch actions for fetching a job detail

--- src/pages/JobDetailPage.jsx
+++ src/pages/JobDetailPage.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import jobActions from '../redux/actions/job.actions';
 
 const JobDetailPage = () => {
   const { id } = useParams();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(jobActions.fetchJob(id));
+  }, [dispatch, id]);
 
   return (
     <div className='JobDetailPage'>
