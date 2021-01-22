c92f692deae80e7bdb45dd73f647e95277a4d9e8
Thang Le Quang <lethang7794@gmail.com>
2021-01-22 12:04:17 +0700

    Use useSelector to get the job from Redux store and show it in JobDetailPage

--- src/pages/JobDetailPage.jsx
+++ src/pages/JobDetailPage.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import jobActions from '../redux/actions/job.actions';
 
 const JobDetailPage = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
+  const job = useSelector((state) => state.job);
   useEffect(() => {
     dispatch(jobActions.fetchJob(id));
   }, [dispatch, id]);
@@ -14,7 +15,12 @@ const JobDetailPage = () => {
   return (
     <div className='JobDetailPage'>
       <Container>
-        <h1>{`Job Detail Page for ${id}`}</h1>
+        {job && (
+          <>
+            <h2>{job.title}</h2>
+            <div>{job.description}</div>
+          </>
+        )}
       </Container>
     </div>
   );
