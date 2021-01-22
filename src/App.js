d58e194f1d1f8db058e748f6409c105466afcadf
Thang Le Quang <lethang7794@gmail.com>
2021-01-22 12:28:22 +0700

    Add error message to JOBS_FETCH_FAILURE action payload and show error message in job list

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useEffect } from 'react';
-import { Container } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import fetchJobs from './redux/actions/job.actions';
 import './App.css';
 import MainNavBar from './components/MainNavBar';
+import JobListPage from './pages/JobListPage';
 
 function App() {
-  const dispatch = useDispatch();
-  const jobs = useSelector((state) => state.jobs);
-  const isLoading = useSelector((state) => state.isLoading);
-  const error = useSelector((state) => state.error);
-
-  useEffect(() => {
-    dispatch(fetchJobs());
-  }, [dispatch]);
   return (
     <div className='App'>
       <MainNavBar />
-      <Container>
-        {error && <h1>{`Something went wrong. Error: ${error}`}</h1>}
-        {isLoading ? (
-          <h1 className='text-center'>Loading</h1>
-        ) : (
-          <ul>
-            {jobs.map((job) => (
-              <li key={job.id}>{job.title}</li>
-            ))}
-          </ul>
-        )}
-      </Container>
+      <JobListPage />
     </div>
   );
 }
---
+++ src/pages/JobListPage.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Container } from 'react-bootstrap';
+import fetchJobs from '../redux/actions/job.actions';
+
+const JobListPage = () => {
+  const dispatch = useDispatch();
+  const jobs = useSelector((state) => state.jobs);
+  const isLoading = useSelector((state) => state.isLoading);
+  const error = useSelector((state) => state.error);
+
+  useEffect(() => {
+    dispatch(fetchJobs());
+  }, [dispatch]);
+
+  return (
+    <Container className='JobList'>
+      {error && (
+        <h1 className='text-center'>{`Something went wrong. Error: ${error}`}</h1>
+      )}
+
+      {isLoading ? (
+        <h1 className='text-center'>Loading</h1>
+      ) : (
+        <ul>
+          {jobs.map((job) => (
+            <li key={job.id}>{job.title}</li>
+          ))}
+        </ul>
+      )}
+    </Container>
+  );
+};
+
+export default JobListPage;
