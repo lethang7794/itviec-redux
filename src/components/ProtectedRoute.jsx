73333be0452246d84f0a1c7464f3eda9c8e030d9
Thang Le Quang <lethang7794@gmail.com>
2021-01-23 15:26:23 +0700

    Add ProtectedRoute component to redirect use to login page if needed

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect, Route } from 'react-router-dom';
+
+const ProtectedRoute = (props) => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  if (isAuthenticated === true) {
+    return <Route {...props} />;
+  }
+
+  return <Redirect to='/login' />;
+};
+
+export default ProtectedRoute;
