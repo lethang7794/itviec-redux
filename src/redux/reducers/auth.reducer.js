e6eecaccc7dd3f59cc16f63eecabf99765581cf8
Thang Le Quang <lethang7794@gmail.com>
2021-01-24 15:29:26 +0700

    Make the authReducer handle LOGIN_INIT, SUCCESS, FAILURE actions

--- src/redux/reducers/auth.reducer.js
+++ src/redux/reducers/auth.reducer.js
@@ -1,8 +1,38 @@
 const initialState = {
   isAuthenticated: false,
+  user: null,
+  isLoading: false,
+  error: null,
 };
 
 const authReducer = (state = initialState, action) => {
+  if (action.type === 'LOGIN_INIT') {
+    return {
+      ...state,
+      isLoading: true,
+      error: null,
+    };
+  }
+
+  if (action.type === 'LOGIN_SUCCESS') {
+    return {
+      ...state,
+      isLoading: false,
+      error: null,
+      isAuthenticated: true,
+      // This is just a mockup for login system
+      user: action.payload,
+    };
+  }
+
+  if (action.type === 'LOGIN_FAILURE') {
+    return {
+      ...state,
+      isLoading: false,
+      error: action.payload,
+    };
+  }
+
   return state;
 };
 
