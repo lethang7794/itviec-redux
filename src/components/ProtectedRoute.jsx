eadc9190164abb6816f5c21f3e4c06f30c668ff0
Thang Le Quang <lethang7794@gmail.com>
2021-01-25 19:00:52 +0700

    Save current location before redirect to Login component
    - After logged in, user will be redirect back to this location.

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route, useLocation } from 'react-router-dom';
 
 const ProtectedRoute = (props) => {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  const location = useLocation();
 
   if (isAuthenticated === true) {
     return <Route {...props} />;
   }
 
-  return <Redirect to='/login' />;
+  return (
+    <Redirect
+      to={{
+        pathname: '/login',
+        state: { from: location.pathname },
+      }}
+    />
+  );
 };
 
 export default ProtectedRoute;
